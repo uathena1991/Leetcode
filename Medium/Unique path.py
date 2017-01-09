af137c2e47076a308df462703b68bea980c270e8
uathena1991 <xiaoli.he@rutgers.edu>
2017-01-09 12:23:23 -0800

    Sorted Array (interesting) + unique path

---
+++ Medium/Unique path.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def uniquePaths(self, m, n):
+        """
+        :type m: int
+        :type n: int
+        :rtype: int
+        """
+        import numpy as np
+        res = [[1]*n]*m # initialize --
+        for i in range(1,m):
+            for j in range(1,n):
+                res[i][j] = res[i-1][j]+res[i][j-1]
+        return res[m-1][n-1]
