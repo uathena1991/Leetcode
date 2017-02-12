8bf4c27de9d0eb63c01c569006315d3b860408b7
lily <uathena@iMac.local>
2017-02-11 22:58:26 -0500

    OA coding parts
    classical question: gray number bit operation!!!

---
+++ Interview coding problems/gray number.py
@@ -0,0 +1,11 @@
+class Solution():
+    # @return a list of integers
+    def grayCode(self, n):
+	    res = []
+	    size = 1<<n
+	    for i in range(size):
+		    res.append((i>>1)^i)
+	    return res
+
+a = Solution()
+a.grayCode(3)
