8a22702fffa1ac2b7fbec7e61ee0c283ad2b1194
lily <uathena@iMac.local>
2017-02-15 17:45:42 -0500

    bug fixed
    1. while res1>0, couldn’t be != 0, because if a = -3, b= 3

--- Interview coding problems/gray number_two numbers.py
+++ Interview coding problems/gray number_two numbers.py
@@ -8,10 +8,11 @@ class Solution(object):
 		"""
 		res1 = x ^ y
 		count = 0
-		while res1 != 0:
+		while res1 > 0:
 			res1,remain = divmod(res1,2)
 			if remain == 1:
 				count += 1
 		return count == 1
 
 a = Solution()
+print a.isgray(-0,1)
