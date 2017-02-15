583da34cba2cced82b1bb87e7296ccf2b4bf5c6b
lily <uathena@iMac.local>
2017-02-15 17:22:14 -0500

    gray number
    1. judge whether two numbers are gray numbers
    2. generate all gray numbers given n

--- Interview coding problems/gray number.py
+++ Interview coding problems/gray number.py
@@ -2,10 +2,10 @@ class Solution():
     # @return a list of integers
     def grayCode(self, n):
 	    res = []
-	    size = 1<<n
+	    size = 2**n
 	    for i in range(size):
 		    res.append((i>>1)^i)
 	    return res
 
 a = Solution()
-a.grayCode(3)
+print a.grayCode(13)
