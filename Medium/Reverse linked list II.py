6dd1268c27e39f3c7ae56c96432962b5e916db66
uathena1991 <xiaoli.he@rutgers.edu>
2018-03-18 21:15:13 -0400

    Review...
    Dump head is not dump…

--- Medium/Reverse linked list II.py
+++ Medium/Reverse linked list II.py
@@ -48,44 +48,48 @@ class Solution(object):
         start_node.next = tmp
 
         return dull_head.next
-# second solution
-# class Solution(object):
-#     def reverseBetween(self, head, m, n):
-#         """
-#         :type head: ListNode
-#         :type m: int
-#         :type n: int
-#         :rtype: ListNode
-#         """
-#         if m == n:
-#             return head
-#         dull_head = ListNode(-1)
-#         dull_head.next = head
-#         pre = dull_head
-#         curr = head
-#         idx = 0
-#         while idx <= n-1 and curr:
-#             if idx == m - 1: # record the last node before [m,n]
-#                 start_prenode = pre
-#                 start_node = curr
-#                 pre = curr
-#                 curr = curr.next
-#             elif idx > m - 1: #  reverse
-#                 tmp = curr.next
-#                 curr.next = pre
-#                 pre = curr
-#                 curr = tmp
-#             else:
-#                 pre = curr
-#                 curr = curr.next
-#             idx += 1
-#         start_prenode.next = pre
-#         start_node.next = curr
-#         return dull_head.next
 
+# second solution
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def reverseBetween(self, head, m, n):
+        """
+        :type head: ListNode
+        :type m: int
+        :type n: int
+        :rtype: ListNode
+        """
+        dump_head = ListNode(0)
+        dump_head.next = head
+        idx = 1
+        pre = dump_head
+        curr = head
+        while idx <= n and curr: #???
+            if idx == m: # record old start node
+                ostart = pre
+                ofirst = curr
+            if idx > m: # reverse
+                nnext = curr.next
+                curr.next = pre
+                pre = curr
+                curr = nnext
+            else:
+                pre = curr
+                curr = curr.next
+            idx += 1
+        # print ostart.val, ofirst.val, pre.val, curr.val
+        ostart.next = pre
+        ofirst.next = curr
+
+        return dump_head.next
 
 
 a = Solution()
-head = leetlib.create_listnode([1,2,3,4,5])
-leetlib.printout_listnode(head)
-leetlib.printout_listnode(a.reverseBetween(head,3,4))
+head = leetlib.ListNode(3).create_listnode([1,2,3,4,5])
+leetlib.ListNode(3).printout_listnode(head)
+leetlib.ListNode(3).printout_listnode(a.reverseBetween(head,3,4))
