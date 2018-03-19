bef65418fa04cd68d2f20051a862d85b24dc47df
uathena1991 <xiaoli.he@rutgers.edu>
2018-03-18 22:17:48 -0400

    tricky math problems

--- Medium/Pow_x_n.py
+++ Medium/Pow_x_n.py
@@ -43,6 +43,21 @@ class Solution(object):
         else:
             return x*self.myPow(x,n-1)
 
+# x ** n = (x*x) ** (n/2)
+class Solution(object):
+    def myPow(self, x, n):
+        """
+        :type x: float
+        :type n: int
+        :rtype: float
+        """
+        if n == 0:
+            return 1
+        if n < 0:
+            n = -n
+            x = 1/x
+        return self.myPow(x*x, n/2) if n%2 == 0 else x*self.myPow(x*x, n/2)
+
 a=Solution()
 t = a.myPow(0.0001,2147483647)
 print t
